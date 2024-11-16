f6d32b6a9c2ad3c1ab74469a6d4293262a0c6d09
Tim Chambers <tim@timchambers.com>
2024-11-16 11:36:08 -0500

    First iteration to insert thumbnails into print view

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,4 +1,36 @@
-// Your bookmarklet code here
 (function () {
-    alert("Hello from my bookmarklet!");
+    // Add header column
+    const headerRow = document.querySelector(
+        "table.Polaris-DataTable__Table thead tr",
+    );
+    const headerCell = headerRow.firstChild.cloneNode(false);
+    headerCell.textContent = "Image";
+    headerRow.prepend(headerCell);
+
+    // Add footer column
+    const footerRow = document.querySelector(
+        "table.Polaris-DataTable__Table tfoot tr",
+    );
+    const footerCell = footerRow.firstChild.cloneNode(false);
+    footerRow.prepend(footerCell);
+
+    // Find Thumbnails
+    let thumbnails = document.querySelectorAll("[class^='_Thumbnail']");
+
+    // Insert row columns
+    document.querySelectorAll(
+        ".Polaris-DataTable__TableRow",
+    ).forEach((tr, i) => {
+        const th = tr.firstChild.cloneNode(false);
+        const thumbnail = thumbnails[i].cloneNode(true);
+        thumbnail.setAttribute("class", "");
+        thumbnail.querySelector("span.Polaris-Thumbnail").classList.replace(
+            "Polaris-Thumbnail--sizeSmall",
+            "Polaris-Thumbnail--sizeLarge",
+        );
+        th.appendChild(thumbnail);
+        tr.prepend(th);
+    });
+
+    window.print();
 })();
